9cab695d844bed2e48eaa682763ef08f10843906
calebcabob <calphonic@gmail.com>
2019-12-23 12:19:47 -0500

    blueline: merge remaining blueline parts with crosshatch tree
    This just makes sense in my opinion
    
    Originated from:
    https://github.com/Aqua-devices/device_google_blueline/commit/e1f8a75442b05711828dfaec77034cbd367a0697
    
    Signed-off-by: calebcabob <calphonic@gmail.com>
    Change-Id: Iadb1bc6c63a0cfb8380a2f539c6db020ec7fe2e1

---
+++ blueline/aquarios.mk
@@ -0,0 +1,47 @@
+# Copyright (C) 2019 AquariOS
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#
+# This file is the build configuration for an aosp Android
+# build for flounder hardware. This cleanly combines a set of
+# device-specific aspects (drivers) with a device-agnostic
+# product configuration (apps). Except for a few implementation
+# details, it only fundamentally contains two inherit-product
+# lines, aosp and blueline, hence it's name.
+
+# Include AquariOS common configurations
+include vendor/aquarios/config/aqua_full_phone.mk
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, device/google/crosshatch/aosp_blueline.mk)
+
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+
+PRODUCT_NAME := aqua_blueline
+PRODUCT_DEVICE := blueline
+PRODUCT_BRAND := Google
+PRODUCT_MODEL := Pixel 3
+PRODUCT_MANUFACTURER := Google
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME="blueline" \
+    TARGET_DEVICE="blueline" \
+    PRIVATE_BUILD_DESC="blueline-user 10 QQ1A.191205.008 5974827 release-keys"
+
+BUILD_FINGERPRINT := "google/blueline/blueline:10/QQ1A.191205.008/5974827:user/release-keys"
+
+$(call inherit-product-if-exists, vendor/google/blueline/blueline-vendor.mk)
+$(call inherit-product-if-exists, vendor/pixelgapps/pixel-gapps.mk)
